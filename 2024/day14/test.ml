6a6cacbad196a59a4b10f940d31631851d364ffd
Chukwuma Akunyili <chukwumaakunyili@gmail.com>
2025-06-14 01:43:17 +0100

    refactor: Clean up and format Day14 code for improved readability

--- 2024/day14/test.ml
+++ 2024/day14/test.ml
@@ -1,35 +1,31 @@
 open OUnit2
 open Day14
 
-let example_input = "p=0,4 v=3,-3
-p=6,3 v=-1,-3
-p=10,3 v=-1,2
-p=2,0 v=2,-1
-p=0,0 v=1,3
-p=3,0 v=-2,-2
-p=7,6 v=-1,-3
-p=3,0 v=-1,-2
-p=9,3 v=2,3
-p=7,3 v=-1,2
-p=2,4 v=2,-3
-p=9,5 v=-3,-3"
+let example_input =
+  "p=0,4 v=3,-3\n\
+   p=6,3 v=-1,-3\n\
+   p=10,3 v=-1,2\n\
+   p=2,0 v=2,-1\n\
+   p=0,0 v=1,3\n\
+   p=3,0 v=-2,-2\n\
+   p=7,6 v=-1,-3\n\
+   p=3,0 v=-1,-2\n\
+   p=9,3 v=2,3\n\
+   p=7,3 v=-1,2\n\
+   p=2,4 v=2,-3\n\
+   p=9,5 v=-3,-3"
 
-let make_part1_test name expected_output (robots, width, height) = 
-  name >:: (fun _ -> 
-    assert_equal expected_output (part1 (robots, width, height)) 
-      ~printer:string_of_int)
+let make_part1_test name expected_output (robots, width, height) =
+  name >:: fun _ ->
+  assert_equal expected_output
+    (part1 (robots, width, height))
+    ~printer:string_of_int
 
-let part1_tests = [
-  make_part1_test "example_part1" 12 (example_input |> parse, 7, 11);
-]
+let part1_tests =
+  [ make_part1_test "example_part1" 12 (example_input |> parse, 7, 11) ]
 
-let suite = "Day14 Test Suite" >::: [
-  "part1 tests" >::: part1_tests;
-]
-
+let suite = "Day14 Test Suite" >::: [ "part1 tests" >::: part1_tests ]
 let () = run_test_tt_main suite
-
-
 
 (* Main entry for day 14 algorithm *)
 let () =
@@ -39,7 +35,7 @@ let () =
 
   (robots, 101, 103) |> part1 |> Printf.printf "Part 1: %d\n";
 
-  (robots, 101, 103) |> part2 |> Printf.printf "Part 2: %d\n"; 
+  (robots, 101, 103) |> part2 |> Printf.printf "Part 2: %d\n";
 
   let end_time = Unix.gettimeofday () in
-  Printf.printf "Elapsed time: %.4f seconds\n" (end_time -. start_time);
+  Printf.printf "Elapsed time: %.4f seconds\n" (end_time -. start_time)
